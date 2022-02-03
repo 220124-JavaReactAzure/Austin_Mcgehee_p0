c0ba02c6b0b4e127cb2df6c36b019ecd8d973beb
Austin07151 <94205817+AGameZzz01@users.noreply.github.com>
2022-02-03 14:58:57 -0600

    Created project0 as a maven project and added MainDriver and AppState

---
+++ Project0/src/main/java/com/revature/project0/driver/MainDriver.java
@@ -0,0 +1,14 @@
+package com.revature.project0.driver;
+
+import com.revature.project0.utils.AppState;
+
+public class MainDriver {
+
+	public static void main(String[] args) {
+		
+		AppState app = new AppState();
+		
+
+	}
+
+}
