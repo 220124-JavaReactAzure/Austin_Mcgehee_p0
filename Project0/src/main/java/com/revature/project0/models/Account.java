df0c3bf00a894c749fb48a305ec9e0a3b83f9448
Austin07151 <94205817+AGameZzz01@users.noreply.github.com>
2022-02-08 10:00:14 -0600

    Added getters and setters to Accounts

--- Project0/src/main/java/com/revature/project0/models/Account.java
+++ Project0/src/main/java/com/revature/project0/models/Account.java
@@ -1,18 +1,19 @@
 package com.revature.project0.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Account implements Serializable {
-	
+
 	private String username;
 	private String password;
 	private int accountId;
 	private int routNum;
-	
+
 	public Account() {
 		super();
 	}
-	
+
 	public Account(String username, String password, int accountId, int routNum) {
 		super();
 		this.username = username;
@@ -20,7 +21,37 @@ public class Account implements Serializable {
 		this.accountId = accountId;
 		this.routNum = routNum;
 	}
-	
-	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public int getRoutNum() {
+		return routNum;
+	}
+
+	public void setRoutNum(int routNum) {
+		this.routNum = routNum;
+	}
 
 }
